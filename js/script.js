f80c85356e2aa7347c1c12fe2b0635d53b6ee75c
antoniojrd <antoniojrfsousadev@gmail.com>
2025-12-19 12:25:35 -0300

    Implement menu item click to close menu
    Added event listeners to menu items to close the menu when clicked.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const toggle =document.getElementById('menu-toggle');
 const menu = document.querySelector('nav ul');
+const menuItens = menu.querySelector('li a');
 
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+menuItens.forEach(item => {
+    item.addEventListener('click', ()=> {
+        menu.classList.remove('active');
+    })
+})
